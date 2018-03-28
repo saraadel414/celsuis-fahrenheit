ae6c6f6957b7f042ad3079ad904a38127829cedd
saraadel414 <36460529+saraadel414@users.noreply.github.com>
2018-03-29 01:05:23 +0200

    Add files via upload

---
+++ pythagoras theorem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double side1 , side2 ;
+    cin>>side1>>side2;
+    double side3=(side1*side1)+(side2*side2) ;
+     side3 =sqrt(side3) ;
+
+    cout <<side3 ;
+
+    return 0;
+
+}
