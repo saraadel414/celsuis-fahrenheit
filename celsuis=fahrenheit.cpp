27f6028366df2fa635d3370b68014859165061dc
saraadel414 <36460529+saraadel414@users.noreply.github.com>
2018-03-29 01:04:53 +0200

    Add files via upload

---
+++ celsuis=fahrenheit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int celsuis=100 ;
+    int fahrenheit=0 ;
+
+    for (fahrenheit=(9*celsuis)/5+32;)
+    cout<< fahrenheit=celsuis;
+
+    return 0;
+}
